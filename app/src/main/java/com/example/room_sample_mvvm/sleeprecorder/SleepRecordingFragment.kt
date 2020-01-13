3f4c976bedb51640d1b6b09f63bd3cf5a1ddc5b0
Manuv <manuram692@gmail.com>
2020-01-13 21:40:40 +0800

    Adding the sleep frsgment and navigation component

--- app/src/main/java/com/example/room_sample_mvvm/sleeprecorder/SleepRecordingFragment.kt
+++ app/src/main/java/com/example/room_sample_mvvm/sleeprecorder/SleepRecordingFragment.kt
@@ -14,10 +14,7 @@ import com.example.room_sample_mvvm.R
  */
 class SleepRecordingFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sleep_recording, container, false)
     }
